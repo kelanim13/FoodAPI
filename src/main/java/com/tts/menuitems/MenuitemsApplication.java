e001b4f54a93f13fef484c04e82debd0634f3b99
kelanim13 <lkmuelle@gmail.com>
2021-04-15 10:36:54 -0500

    adding files

---
+++ src/main/java/com/tts/menuitems/MenuitemsApplication.java
@@ -0,0 +1,13 @@
+package com.tts.menuitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuitemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuitemsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tts/menuitems/model/Ingredient.java
@@ -0,0 +1,9 @@
+package com.tts.menuitems.model;
+
+public class Ingredient 
+{
+    private String name;
+    private String category;
+    private String origin;
+    private boolean isMeat;
+}
